e14b26b6e7fa932cd01b5f9871e1a42686c3d0c4
Guilherme Elias <gpelias@hotmail.com>
2022-05-10 22:40:50 -0300

    adding env properties

---
+++ src/util/DataSourceParse.ts
@@ -0,0 +1,20 @@
+type typeType = 'mysql' | 'mariadb';
+type portType = number;
+
+export const properties: ProcessEnvInterface = {
+    type: <typeType>process.env.TYPE,
+    host: process.env.HOST,
+    port: <portType><unknown>process.env.PORT,
+    username: process.env.LOGIN,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+}
+
+export interface ProcessEnvInterface {
+    type?: typeType;
+    host?: string;
+    port?: number;
+    username?: string;
+    password?: string;
+    database?: string;
+}
